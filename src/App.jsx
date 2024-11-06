126849f37f7d1d7769cebc8761575808c3742006
Fasakin Henry <fasakinhenry@gmail.com>
2024-11-06 16:42:20 +0100

    feat: add the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+import React from 'react';
+import Header from './components/Header';
+// import HeroSection from './components/HeroSection';
+// import PartnerSection from './components/PartnerSection';
+// import FinancialSection from './components/FinancialSection';
+// import UnlockingSection from './components/UnlockingSection';
+// import TestimonialSection from './components/TestimonialSection';
+// import FAQSection from './components/FAQSection';
+// import CTASection from './components/CTASection';
+// import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div className='font-instrumentSans'>
+      <Header />
+      {/* <HeroSection />
+      <PartnerSection />
+      <FinancialSection />
+      <UnlockingSection />
+      <TestimonialSection />
+      <FAQSection />
+      <CTASection />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+// import { ReactComponent as Logo } from '../assets/logo-desktop.svg';
+
+const Header = () => {
+  return (
+    <header className='bg-white shadow'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex justify-between h-16'>
+          <div className='flex'>
+            <div className='flex-shrink-0 flex items-center'>
+              <a href='/'>
+                {/* <Logo className='block lg:hidden h-8 w-auto' />
+                <Logo className='hidden lg:block h-8 w-auto' /> */}
+                <img src='/vite.svg' alt='Mini logo' />
+              </a>
+            </div>
+            <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
+              <a
+                href='#'
+                className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+              >
+                Service
+              </a>
+              <a
+                href='#'
+                className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+              >
+                About us
+              </a>
+              <a
+                href='#'
+                className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+              >
+                Benefits
+              </a>
+              <a
+                href='#'
+                className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+              >
+                FAQ ?
+              </a>
+            </div>
+          </div>
+          <div className='hidden sm:ml-6 sm:flex sm:items-center'>
+            <a
+              href='#'
+              className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'
+            >
+              Book a Strategy Call
+            </a>
+            <a
+              href='#'
+              className='ml-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-300 transition ease-in-out duration-150'
+            >
+              Partner with Us
+            </a>
+          </div>
+          <div className='-mr-2 flex items-center sm:hidden'>
+            <button
+              type='button'
+              className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out'
+            >
+              <svg
+                className='h-6 w-6'
+                stroke='currentColor'
+                fill='none'
+                viewBox='0 0 24 24'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
